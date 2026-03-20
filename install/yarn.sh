4d62898d11841796c46480590b087a6508172965
Brandon Johnson <13704128+darwinz@users.noreply.github.com>
2026-03-19 23:46:36 -0600

    Remove legacy shell scripts, installer, and mycli binary

--- install/yarn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-separator
-yi jest true
--- sources/mongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function mongo_local_start() {
-  mongod >> ~/mongo/output.log 2>&1 &
-}
-
-function mongo_port() {
-  lsof -iTCP -sTCP:LISTEN | grep mongo | awk '{print $9}'
-}
